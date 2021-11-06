23f4288b57314ba9966caf1a98513bfd7be7cc63
Gabriel Grysuk <gabrielgrysuk@gmail.com>
2021-11-06 02:38:22 -0300

    Test mobile navigation

---
+++ js/navigation.js
@@ -0,0 +1,26 @@
+const header = document.getElementById("header");
+const navbar = document.getElementById("navbar");
+const menuLinks = document.getElementById("menu-links");
+const menuBtn = document.getElementById("menu-btn");
+const iconHamburger = document.getElementById("icon-hamburger");
+const iconClose = document.getElementById("icon-close");
+const body = document.getElementById("body");
+
+// function handleMenu() {
+//   iconHamburger.classList.toggle("hidden");
+//   iconClose.classList.toggle("hidden");
+//   body.classList.toggle("menu--show");
+//   header.classList.toggle("menu--show");
+//   navbar.classList.toggle("menu--show");
+//   menuLinks.classList.toggle("menu--show");
+// }
+
+menuBtn.addEventListener("click", () => {
+  console.log("pressed");
+  iconHamburger.classList.toggle("hidden");
+  iconClose.classList.toggle("hidden");
+  body.classList.toggle("menu--show");
+  menuLinks.classList.toggle("menu--show");
+  navbar.classList.toggle("menu--show");
+  header.classList.toggle("menu--show");
+});
